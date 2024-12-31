893fe3897540c3601404f571765217d6572c5138
Muhammad Mahad Siddiqui <120809411+Mahad-siddiqui@users.noreply.github.com>
2024-12-31 18:55:47 +0500

    Delete server directory

--- server/Routes/DashboardRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getUserDetails } from '../Controllers/DashboardController.js';
-import { authenticateToken } from '../Middlewares/AuthMiddleware.js';
-
-const router = express.Router();
-
-router.get('/', authenticateToken, getUserDetails);
-
-export default router;
--- server/guid.md
+++
@@ -1,3 +0,0 @@
-npm init
-npm install express body-parser cors cookie-parser bcrypt jsonwebtoken mongoose dotenv joi
-npm nodemon
